c22c68f1151d78cb31d2aed3a42d4d6cde0a5555
yosulin <yosu.suarez@gmail.com>
2026-06-20 19:54:22 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'juego-circulos-v1';
+const ASSETS = [
+  '/juego-circulos/',
+  '/juego-circulos/index.html',
+  '/juego-circulos/manifest.json',
+  '/juego-circulos/icon-192.png',
+  '/juego-circulos/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
